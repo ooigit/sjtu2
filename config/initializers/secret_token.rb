68d7c0869c1bbe3e198a3a60bc94b4611c29ed48
DongQS <dongqs@gmail.com>
2011-12-20 22:28:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sjtu2::Application.config.secret_token = '0ebd6e6f872b5858657f7ffb52a9654cfe200a65c0899df5f97035820d530a07411e40c6fec84e78bb6a70f2328874b75dca05a470d4dc9164ee2f068efd5464'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sjtu2::Application.config.session_store :cookie_store, key: '_sjtu2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sjtu2::Application.config.session_store :active_record_store
